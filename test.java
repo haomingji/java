6bab659b88c5e9615f7ede792ed7bc288cd8caaf
haomingji <jhm0331@163.com>
2017-03-27 18:08:21 +0800

    字符串数组

---
+++ test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test {
+	public static void main(String[] args){
+		String s1="jihaoming";
+	    
+	    char[] s3={'b','o','y','d','o','g','b'};
+	    String s4=new String(s3);
+	   String[] s2= s1.split("i");
+	   for(int i=0;i<s2.length;i++){
+	   System.out.println(s2[i]+" ");
+	   }
+}
+
+}
+
