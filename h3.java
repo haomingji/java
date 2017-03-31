d040b39f64b56785d848139002559f6c6b7de102
haomingji <jhm0331@163.com>
2017-03-31 14:29:27 +0800

    创建平面坐标类（Point，设置两个成员变量x和y，分别表示该点的两个坐标）

--- h3.java
+++ h3.java
@@ -1,26 +1,87 @@
-package homework14;
+package h15;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class h3 {
-public static void main(String[] args) {
-	Scanner scan=new Scanner(System.in);
-	for(int i=0;;i++){
-		String str=scan.next(); 
-		if(str.equals("exit")){
-			break;
+	public static void main(String[] args) {
+		ArrayList <point> list =new ArrayList<point>(); 
+	    point p1=new point();
+	    p1.x=8;
+	    p1.y=4;
+		list.add(p1);
+		point p2=new point();
+		p2.x=5;
+		p2.y=9;
+		list.add(p2);
+		point p3=new point();
+		p3.x=2;
+		p3.y=6;
+		list.add(p3);
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size()-1; j++) {
+				if(list.get(j).x>list.get(j+1).x){
+					point temp=list.get(j);
+					list.set(j,list.get(j+1));
+					list.set(j+1, temp);
+				}
+			}
 		}
-		if(str.indexOf("a")==0){
-			System.out.print(str);
-			System.out.println();
+		System.out.println("按x升序排列");
+		for (int i = 0; i < list.size(); i++) {
+			point p =list.get(i);
+			System.out.print(p.x+",");
+			System.out.print(p.y+" ");
 		}
-	 if(str.indexOf("b")==0){
-		for(int j=0;j<2;j++){
-			System.out.print(str+" ");
-			System.out.println();
-		}
-		
+		System.out.println();
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size()-1; j++) {
+				if(list.get(j).y<list.get(j+1).y){
+					point temp=list.get(j);
+					list.set(j,list.get(j+1));
+					list.set(j+1, temp);
+				}
+			}
+		}System.out.println("按y降序排列");
+		for (int i = 0; i < list.size(); i++) {
+			point p =list.get(i);
+			System.out.print(p.x+",");
+			System.out.print(p.y+" ");
 	}
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size()-1; j++) {
+				if(getDistance(list.get(j).x,list.get(j).y)>getDistance(list.get(j+1).x,list.get(j+1).y)){
+					point temp=list.get(j);
+				    list.set(j,list.get(j+1));
+				    list.set(j+1, temp);
+				}
+			}
+			}
+		System.out.println();
+		System.out.println("按距离原点距离升序输出");
+		for (int i = 0; i < list.size(); i++) {
+			point p =list.get(i);
+			System.out.print(p.x+",");
+			System.out.print(p.y+" ");
+		}
+			
+		}
+			
+			
+		
+
+
+	 public static double getDistance(double x,double y){
+		  return Math.sqrt(x*x+y*y);
 }
 }
+
+
+
+
+
+
+
+class  point {
+	 int x;
+	 int y;
 }
