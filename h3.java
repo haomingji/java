1e1eb3d039126beb9ebcb8ac6ab506c2fb029a1f
haomingji <jhm0331@163.com>
2017-04-07 13:47:53 +0800

    用hashset存储水果，根据产地和单价判断

--- h3.java
+++ h3.java
@@ -1,87 +1,46 @@
-package h15;
+package homework17;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class h3 {
 	public static void main(String[] args) {
-		ArrayList <point> list =new ArrayList<point>(); 
-	    point p1=new point();
-	    p1.x=8;
-	    p1.y=4;
-		list.add(p1);
-		point p2=new point();
-		p2.x=5;
-		p2.y=9;
-		list.add(p2);
-		point p3=new point();
-		p3.x=2;
-		p3.y=6;
-		list.add(p3);
-		for (int i = 0; i < list.size(); i++) {
-			for (int j = 0; j < list.size()-1; j++) {
-				if(list.get(j).x>list.get(j+1).x){
-					point temp=list.get(j);
-					list.set(j,list.get(j+1));
-					list.set(j+1, temp);
-				}
-			}
-		}
-		System.out.println("按x升序排列");
-		for (int i = 0; i < list.size(); i++) {
-			point p =list.get(i);
-			System.out.print(p.x+",");
-			System.out.print(p.y+" ");
-		}
-		System.out.println();
-		for (int i = 0; i < list.size(); i++) {
-			for (int j = 0; j < list.size()-1; j++) {
-				if(list.get(j).y<list.get(j+1).y){
-					point temp=list.get(j);
-					list.set(j,list.get(j+1));
-					list.set(j+1, temp);
-				}
-			}
-		}System.out.println("按y降序排列");
-		for (int i = 0; i < list.size(); i++) {
-			point p =list.get(i);
-			System.out.print(p.x+",");
-			System.out.print(p.y+" ");
-	}
-		for (int i = 0; i < list.size(); i++) {
-			for (int j = 0; j < list.size()-1; j++) {
-				if(getDistance(list.get(j).x,list.get(j).y)>getDistance(list.get(j+1).x,list.get(j+1).y)){
-					point temp=list.get(j);
-				    list.set(j,list.get(j+1));
-				    list.set(j+1, temp);
-				}
-			}
-			}
-		System.out.println();
-		System.out.println("按距离原点距离升序输出");
-		for (int i = 0; i < list.size(); i++) {
-			point p =list.get(i);
-			System.out.print(p.x+",");
-			System.out.print(p.y+" ");
-		}
-			
-		}
-			
-			
+		HashSet<Fruit> hs = new HashSet<Fruit>();
+		Fruit f1=new Fruit("china", "apple", 10);
+		Fruit f2=new Fruit("America", "peer", 100);
+		Fruit f3=new Fruit("England", "juice", 25);
+		Fruit f4=new Fruit("Korea", "apple", 10);
 		
+		hs.add(f1);
+		hs.add(f2);
+		hs.add(f3);
+		hs.add(f4);
+		Iterator<Fruit> it=hs.iterator();
+		while(it.hasNext()){
+			Fruit fruit=it.next();
+			System.out.println(fruit.place+" "+fruit.name+" "+fruit.price);
+		}
+		
+		
+		
+	}
 
-
-	 public static double getDistance(double x,double y){
-		  return Math.sqrt(x*x+y*y);
 }
+class Fruit{
+	String name;
+	int price;
+	String place;
+	public Fruit(String place,String name,int price) {
+		super();
+		this.name=name;
+		this.price=price;
+		this.place = place;
+	}
+	public int hashCode(){
+		return price;
+	}
+public boolean equals(Object obj){
+	Fruit f = (Fruit) obj;
+	return f.place==this.place;
 }
-
-
-
-
-
-
-
-class  point {
-	 int x;
-	 int y;
 }
