3cc98cdc7c3380b95bdece045c1b8ee76f8cc547
haomingji <jhm0331@163.com>
2017-04-07 13:46:25 +0800

    用hashset存相似三角形方法一

--- h1.java
+++ h1.java
@@ -1,66 +1,44 @@
-package honmework16;
+package homework17;
 
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.function.IntToDoubleFunction;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class h1 {
 	public static void main(String[] args) {
-		LinkedList<Integer> list=new LinkedList<Integer>();
-		LinkedList<Integer> outlist=new LinkedList<Integer>();
+		HashSet<Triangle> hs=new HashSet<Triangle>();
+		Triangle t1= new Triangle(20,60);
+		Triangle t2= new Triangle(20,60);
+		Triangle t3= new Triangle(30,60);
+		hs.add(t1);
+		hs.add(t2);
+		hs.add(t3);
+	 Iterator<Triangle> it=hs.iterator();
+	 while( it.hasNext()){
+		 Triangle t= it.next();
+		 System.out.println(t.a+" "+t.b);
+	 }
 		
-		for (int i = 0; i <4; i++) {
-			int a =(int) (Math.random() * 10);
-		    	list.add(i, a);
-		}
-		for (int i = 0; i < list.size(); i++) {
-			System.out.print(list.get(i)+" ");
-			
-		}
-		for (int j = 0; j< 10; j++) {
-			System.out.println("输入四个数，还有"+(10-j-1)+"次");
-			if( j<9){
-		
-		
-		Scanner scan=new Scanner(System.in);
-		for (int i = 0; i < 4; i++) {
-			int m =scan.nextInt();
-			
-			outlist.add(i,m);
-		}int temp=0;
-		for (int i = 0; i < 4; i++) {
-			if( outlist.get(i)==list.get(i)){
-				temp++;
-			}
-		}if(temp==4){
-			System.out.println("恭喜你答对了");
-			break;
-			}
-		else {
-			System.out.println("很遗憾答错了，但有"+temp+"个数字正确");
-		}
-		System.out.println("是否查看之前的输入");
-		String b= scan.next();
-		if(b.equals("是")){
-		for (int i = 0; i < outlist.size(); i++) {
-			
-			if(i%4==0){
-				System.out.print("\n"+ outlist.get(i)+" ");
-				
-			}else{
-				System.out.print(outlist.get(i)+" ");
-			}
-		}
-		System.out.println();
-		}
-	}if(j==9){
-		System.out.println("猜测失败");
+	
+}
+
+}
+ class  Triangle{
+	int a;
+	int b;
+	
+	 public Triangle(int a, int b) {
+	super();
+	this.a = a;
+	this.b = b;
+}
+	
+	public int hashCode() {
+		return a;
 	}
-		
-		}	
-		
-		}
-		
+	public boolean equals(Object obj){
+		Triangle t= (Triangle) obj;
+		return t.b ==this.b;
 	}
+}
 
 
