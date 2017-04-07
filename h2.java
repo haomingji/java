ade67901ed1d317104aa121c884e2d052d0d2fc1
haomingji <jhm0331@163.com>
2017-04-07 13:46:58 +0800

    用hashset存相似三角形方法二

--- h2.java
+++ h2.java
@@ -1,32 +1,54 @@
-package h15;
+package homework17;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
-public class h2 { 
+public class h2 {
 	public static void main(String[] args) {
-		T t =new T();
-		t.addandsort(9);
+		HashSet<Triangle1> hs=new HashSet<Triangle1>();
+		Triangle1 t4=new Triangle1(30,4,5,6);
+		Triangle1 t5=new Triangle1(20,2,3,6);
+		Triangle1 t6=new Triangle1(60,8,10,12);
+		Triangle1 t7=new Triangle1(30,13,10,13);
+		hs.add(t4);
+		hs.add(t5);
+		hs.add(t6);
+		hs.add(t7);
+		Iterator<Triangle1> it=hs.iterator();
+		while(it.hasNext()){
+              Triangle1 t= it.next();
+              System.out.println(t.a+" "+t.x+" "+t.y+" "+t.z);
+		}
+		
+		
 	}
 
 }
-class T{
-	ArrayList<Integer > alist =new ArrayList<Integer > ();
-	public void addandsort(int element){
-		alist.add(1);
-		alist.add(3);
-		alist.add(5);
-		alist.add(6);
-		alist.add(element);
-		for (int i = 0; i < alist.size(); i++) {
-			for (int j = 0; j < alist.size()-1-i; j++) {
-				if(alist.get(j)<alist.get(j+1)){
-					int temp=alist.get(j);
-					alist.set(j, alist.get(j+1));
-					alist.set(j+1, temp);
-				}
-			}
-		}for (int i = 0; i < alist.size(); i++) {
-			System.out.print(alist.get(i)+" ");
-		}
+class Triangle1{
+	int  a;//xÓëz±ßµÄ¼Ð½Ç
+	int b;
+	int x;
+	int y;
+	int z;
+	public Triangle1(int a,int x, int y, int z) {
+		super();
+		this.a=a;
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
+	public  int hashCode(){
+		
+			return  a  ;
+		
+		
+	}
+	public boolean equals(Object obj){
+		Triangle1 t=(Triangle1) obj;
+		return t.x*this.z==t.z*this.x;
+			
+		
+		
+	}
+	
 }
