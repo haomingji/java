736f388e3341783ceb1ee9102b53ba70fe2ee939
纪浩铭 <jhm0331@163.com>
2017-08-31 15:26:24 +0800

    替换空格
    请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。

---
+++ niuke0831.java
@@ -0,0 +1,19 @@
+package test2;
+
+public class niuke0831 {
+	public String replaceSpace(StringBuffer str) {
+
+		String sti = str.toString();
+		char[] strChar = sti.toCharArray();
+		StringBuffer stb = new StringBuffer();
+		for (int i = 0; i < strChar.length; i++) {
+			if (strChar[i] == ' ') {
+				stb.append("%20");
+			} else {
+				stb.append(strChar[i]);
+			}
+		}
+		return stb.toString();
+	}
+
+}
